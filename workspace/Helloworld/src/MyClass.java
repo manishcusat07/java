b09fee5b1bd7e4ac2d81c525699bd5fbbe793b88
Manish <manish.cusat07@gmail.com>
2020-01-31 00:35:34 +0530

    "Initial commit"

---
+++ workspace/Helloworld/src/MyClass.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class MyClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=2;
+		int c;
+		c=a+b;
+		System.out.println("Sum is "+ c);
+
+	}
+
+}
---
+++ workspace/Helloworld/src/scm/Colour.java
@@ -0,0 +1,9 @@
+package scm;
+
+public class Colour {
+
+	public Colour() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
